0a7e52d51e058ea550369a4b95f7f4e8f6a04d20
David-Salazar-1082 <164534046+David-Salazar-1082@users.noreply.github.com>
2025-04-08 12:13:00 -0600

    Add files via upload

---
+++ lib/pagina_dos.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class PantallaDos extends StatelessWidget {
+  const PantallaDos({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange, // Fondo azul
+        title: const Center(
+          child: Text(
+            'Pantalla 2',
+            style: TextStyle(
+              color: Colors.white, // Letra blanca
+              fontSize: 20.0, // Tamaño de la letra 20
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context, '/pantalla1');
+          },
+          child: const Text('Pantalla 1'),
+        ),
+      ),
+    );
+  }
+}
